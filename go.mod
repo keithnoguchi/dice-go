1bb545eb5eeccdf441ed284618c3a795b850f7e5
Keith Noguchi <keith@noguchi.us>
2023-10-26 17:17:58 -0700

    main.go: Start to instrument the HTTP request!
    This is like a magic but it's a good starting point
    how to do the instrumentation in a right, or I would
    say, the standard way.

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module dice-go
 go 1.21.0
 
 require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.45.0 // indirect
 	go.opentelemetry.io/otel v1.19.0 // indirect
 	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v0.42.0 // indirect
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.19.0 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"errors"
 	"log"
-	//"net/http"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 func main() {
@@ -32,12 +36,50 @@ func run() (err error) {
 		err = errors.Join(err, otelShutdown(context.Background()))
 	}()
 
+	// start HTTP server
+	srv := &http.Server{
+		Addr: ":8080",
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout: time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler: newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
+
 	// main loop
 	select {
+	case err = <-srvErr:
+		return
 	case <-ctx.Done():
 		// waiting for first Ctrl+C.
 		// stop receiving signal notifications as soon as possible
 		stop()
-		return
 	}
+
+	// When Shutdown is called, ListenAndServ immediately returns
+	// ErrServerClosed.
+	err = srv.Shutdown(context.Background())
+	return
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	// handleFunc is a replacement for mux.HandleFunc
+	// which enriches the handler's HTTP instrumentation with the pattern
+	// as the HTTP route.
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		// Configure the "http.route" for the HTTP instrumentation
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
+	// register the handlers
+	handleFunc("/rolldice", rolldice)
+
+	// add HTTP instrumentation for the whole server.
+	return otelhttp.NewHandler(mux, "/")
 }
